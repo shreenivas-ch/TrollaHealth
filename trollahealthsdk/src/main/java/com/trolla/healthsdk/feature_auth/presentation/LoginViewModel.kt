5f43b927991f1e0ae53c28bd10617a8baf64e915
shreenivas <shreenivasch@gmail.com>
2022-04-16 01:12:08 +0530

    initial commit 3

--- trollahealthsdk/src/main/java/com/trolla/healthsdk/feature_auth/presentation/LoginViewModel.kt
+++ trollahealthsdk/src/main/java/com/trolla/healthsdk/feature_auth/presentation/LoginViewModel.kt
@@ -1,8 +1,25 @@
 package com.trolla.healthsdk.feature_auth.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.trolla.healthsdk.data.Resource
 import com.trolla.healthsdk.feature_auth.domain.usecases.LoginUseCase
+import kotlinx.coroutines.launch
 
-class LoginViewModel(loginUseCase: LoginUseCase): ViewModel() {
+class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
 
+    fun login() {
+        viewModelScope.launch {
+
+            when (val result = loginUseCase()) {
+                is Resource.Success -> {
+                    Log.e("EV----->", "Success")
+                }
+                is Resource.Error -> {
+
+                }
+            }
+        }
+    }
 }
